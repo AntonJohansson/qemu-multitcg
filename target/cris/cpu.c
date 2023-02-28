17a342a8889a0d042c5b1e255436caeb1fd15193
Anton Johansson <anjo@rev.ng>
2023-02-28 13:24:24 +0100

    target/cris: Set `tcg_ctx->long_bits` in `cpu_realizefn`
    Signed-off-by: Anton Johansson <anjo@rev.ng>

--- target/cris/cpu.c
+++ target/cris/cpu.c
@@ -146,6 +146,8 @@ static void cris_cpu_realizefn(DeviceState *dev, Error **errp)
     CRISCPUClass *ccc = CRIS_CPU_GET_CLASS(dev);
     Error *local_err = NULL;
 
+    tcg_ctx->long_bits = 32;
+
     cpu_exec_realizefn(cs, &local_err);
     if (local_err != NULL) {
         error_propagate(errp, local_err);
