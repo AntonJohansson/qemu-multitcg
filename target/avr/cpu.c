88c461a596872c9c3e64ed96698887ad9bd53464
Anton Johansson <anjo@rev.ng>
2023-02-28 13:24:16 +0100

    target/avr: Set `tcg_ctx->long_bits` in `cpu_realizefn`
    Signed-off-by: Anton Johansson <anjo@rev.ng>

--- target/avr/cpu.c
+++ target/avr/cpu.c
@@ -113,6 +113,8 @@ static void avr_cpu_realizefn(DeviceState *dev, Error **errp)
     AVRCPUClass *mcc = AVR_CPU_GET_CLASS(dev);
     Error *local_err = NULL;
 
+    tcg_ctx->long_bits = 32;
+
     cpu_exec_realizefn(cs, &local_err);
     if (local_err != NULL) {
         error_propagate(errp, local_err);
